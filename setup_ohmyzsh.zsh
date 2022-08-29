d3669c519161135467dec84bb18c58910ba10892
Minghao Xu <85087583+xu-minghao317@users.noreply.github.com>
2022-08-29 14:17:21 +0900

    style: add double quote to env variables

--- setup_ohmyzsh.zsh
+++ setup_ohmyzsh.zsh
@@ -4,7 +4,7 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Source oh-my-zsh to get variables
-source $ZSH/oh-my-zsh.sh
+source "$ZSH"/oh-my-zsh.sh
 
 # Install spaceship prompt
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM"/themes/spaceship-prompt --depth=1 && \
@@ -15,5 +15,4 @@ git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins "$ZSH_CUSTOM"/
 git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM"/plugins/zsh-autosuggestions --depth=1
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM"/plugins/zsh-syntax-highlighting --depth=1
 
-rm -f $HOME/.zshrc.pre-oh-my-zsh
-source $HOME/.zshrc
+rm -f "$HOME"/.zshrc.pre-oh-my-zsh
