2cbc858001fdd7b20a091b2b700afd8ec2b11388
Minghao Xu <85087583+xu-minghao317@users.noreply.github.com>
2022-09-03 22:16:11 +0900

    fix: remove `NONINTERACTIVE=1` (#5)

--- setup_brew.zsh
+++ setup_brew.zsh
@@ -1,8 +1,7 @@
 #!/usr/bin/env zsh
 
-# Install Homebrew non-interactively without prompting for passwords
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval $(/opt/homebrew/bin/brew shellenv)
 
 brew analytics off
-brew bundle --verbose && brew cleanup && brew autoremove
+brew bundle --verbose && brew cleanup
