db3a0a5032438d22dd6f62c8fd7d70c9ec1cfeb0
Minghao Xu <85087583+xu-minghao317@users.noreply.github.com>
2022-10-02 22:39:37 +0900

    fix: remove spaceship

--- setup_ohmyzsh.zsh
+++ setup_ohmyzsh.zsh
@@ -6,14 +6,8 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # Source oh-my-zsh to get variables
 source "$ZSH"/oh-my-zsh.sh
 
-# Install spaceship prompt (now use starship through brew)
-# git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM"/themes/spaceship-prompt --depth=1 && \
-# ln -s "$ZSH_CUSTOM"/themes/spaceship-prompt/spaceship.zsh-theme "$ZSH_CUSTOM"/themes/spaceship.zsh-theme
 # Install plugins
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git "$ZSH_CUSTOM"/plugins/you-should-use --depth=1
 git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins "$ZSH_CUSTOM"/plugins/autoupdate --depth=1
 git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM"/plugins/zsh-autosuggestions --depth=1
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM"/plugins/zsh-syntax-highlighting --depth=1
-
-# Remove archieved zshrc
-rm -f "$HOME"/.zshrc.pre-oh-my-zsh
