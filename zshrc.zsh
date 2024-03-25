0c9cc08cddaf0333448ec94dd180eaa4b246694d
xu-minghao317 <85087583+xu-minghao317@users.noreply.github.com>
2024-03-25 18:58:44 +0900

    chore: temporarily remove Linux conda init

--- zshrc.zsh
+++ zshrc.zsh
@@ -96,22 +96,9 @@ if [[ "$OS_TYPE" == "Darwin" ]]; then
         eval "$__conda_setup"
     else
         if [ -f "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
-            . "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh"
+            source "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh"
         else
-            export PATH="/opt/homebrew/Caskroom/miniconda/base/bin:$PATH"
-        fi
-    fi
-    unset __conda_setup
-
-elif [[ "$OS_TYPE" == "Linux" ]]; then
-    __conda_setup="$('/opt/homebrew/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2>/dev/null)"
-    if [ $? -eq 0 ]; then
-        eval "$__conda_setup"
-    else
-        if [ -f "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
-            . "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh"
-        else
-            export PATH="/opt/homebrew/Caskroom/miniconda/base/bin:$PATH"
+            path=("/opt/homebrew/Caskroom/miniconda/base/bin" $path)
         fi
     fi
     unset __conda_setup
