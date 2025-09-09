7e0bd4db59e7433dc8d40b3b7c1033fea8611b4f
Ming-Hao Xu <85087583+ming-hao-xu@users.noreply.github.com>
2025-09-09 16:20:39 +0900

    feat: Add host-specific AWS environment variables
    This commit adds a condition to `zshrc.zsh` to set AWS-related environment variables based on the hostname.

--- zshrc.zsh
+++ zshrc.zsh
@@ -103,6 +103,13 @@ export _ZO_FZF_OPTS="
     --height=10%
     --layout=reverse"
 
+# [freee] AWS
+if [[ "$SHORT_HOST" == "D394J047WT" ]]; then
+    export AWS_REGION=ap-northeast-1
+    export AWS_PROFILE=freee
+    export ONELOGIN_MFA_IP_ADDRESS=$(curl -SsL http://checkip.amazonaws.com/)
+fi
+
 ### Secrets ###
 # This file is not committed to git
 [[ -f "$HOME/.dotfiles/secrets.zsh" ]] && source "$HOME/.dotfiles/secrets.zsh"
