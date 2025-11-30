911d99bddfa59b35091f4dda20b8fed8f7d1fe91
Minghao Xu <85087583+ming-hao-xu@users.noreply.github.com>
2025-11-30 23:20:50 +0900

    refactor: ensure user-local binaries take precedence in PATH

--- config/zsh/zprofile.zsh
+++ config/zsh/zprofile.zsh
@@ -1,11 +1,11 @@
-# Include $HOME/.local/bin if it exists
-if [[ -d "$HOME/.local/bin" ]]; then
-    path=("$HOME/.local/bin" $path)
-fi
-
 ### Homebrew ###
 if [[ "$OSTYPE" == darwin* ]]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+# Include $HOME/.local/bin if it exists
+if [[ -d "$HOME/.local/bin" ]]; then
+    path=("$HOME/.local/bin" $path)
 fi
 
 # Set neovim as default editor
